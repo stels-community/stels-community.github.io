cdc409efcf9aae017e22ce11304ed8b24a83da22
Evgeny Agafonov <eagafonov@spbdev.ru>
2018-12-07 15:54:00 +0300

    trim spaces

---
+++ precache-manifest.4bd9be904be97fdb98983641bf5ed342.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aaa322823534f27395a2349017596715",
+    "url": "/static/media/logo-te-logo.aaa32282.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ecc610280e77f79a3ea6",
+    "url": "/static/js/main.ecc61028.chunk.js"
+  },
+  {
+    "revision": "6d592f7e66629b31a35b",
+    "url": "/static/js/1.6d592f7e.chunk.js"
+  },
+  {
+    "revision": "ecc610280e77f79a3ea6",
+    "url": "/static/css/main.6d15bf41.chunk.css"
+  },
+  {
+    "revision": "76618eeac2e49d44e7a82948b4700dea",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aeb97ac6f876416129996fadd3dfeafe.js"
+  "/precache-manifest.4bd9be904be97fdb98983641bf5ed342.js"
 );
 
 workbox.clientsClaim();
