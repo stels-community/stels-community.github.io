e2146888e5f16f9f015dd94089169dcadcc9720a
Evgeny Agafonov <eagafonov@spbdev.ru>
2019-12-07 16:07:41 +0300

    tabs style

--- precache-manifest.86eaacace70e5eb82eb0dbe39f17078f.js
+++ precache-manifest.86eaacace70e5eb82eb0dbe39f17078f.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "54d83141eca9c11b425f",
-    "url": "/static/js/main.54d83141.chunk.js"
+    "revision": "51c4b30b39b089d9beca",
+    "url": "/static/js/main.51c4b30b.chunk.js"
   },
   {
     "revision": "4e997617b0287ef900cc",
     "url": "/static/js/1.4e997617.chunk.js"
   },
   {
-    "revision": "54d83141eca9c11b425f",
-    "url": "/static/css/main.161a994a.chunk.css"
+    "revision": "51c4b30b39b089d9beca",
+    "url": "/static/css/main.958cd0ca.chunk.css"
   },
   {
     "revision": "4e997617b0287ef900cc",
     "url": "/static/css/1.5e9644bf.chunk.css"
   },
   {
-    "revision": "d630b6e6c92802b75fc798102c3186d2",
+    "revision": "41dd1812e83a866f89b5084b663ce75e",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.56a86337bee45aec7e459e0ee1448be4.js"
+  "/precache-manifest.86eaacace70e5eb82eb0dbe39f17078f.js"
 );
 
 workbox.clientsClaim();
